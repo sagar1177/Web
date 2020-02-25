9b8bfffbca7fc4672776fe2193b26e01b4f91c16
SagarPatel <sagar.patel@live.com>
2020-02-25 12:00:44 +0530

    Added SignOn.Web Project

---
+++ Web/Web/GlobalSuppressions.cs
@@ -0,0 +1,5 @@
+﻿// <copyright file="GlobalSuppressions.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.OrderingRules", "SA1200:Using directives must be placed correctly", Justification = "<Pending>")]
